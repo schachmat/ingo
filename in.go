633c923c596dc3a8101f6e284b65b62e33a295fa
Markus Teich <markus.teich@stusta.mhn.de>
2016-02-25 16:48:38 +0100

    add header and auto generate config file name from app name

--- in.go
+++ in.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
+	"os/user"
+	"path"
 	"strings"
 )
 
@@ -13,23 +14,38 @@ var (
 	obsoleteKeys = make(map[string]string)
 )
 
-func Parse(configPath string) {
+func Parse(appName string) error {
 	if flag.Parsed() {
-		log.Fatalf("flags have been parsed already.")
+		return fmt.Errorf("flags have been parsed already.")
 	}
 
-	loadConfig(configPath)
-	saveConfig(configPath)
+	envname := strings.ToUpper(appName) + "RC"
+	cPath := os.Getenv(envname)
+	if cPath == "" {
+		usr, err := user.Current()
+		if err != nil {
+			return fmt.Errorf("%v\nYou can set the environment variable %s to point to your config file as a workaround.", err, envname)
+		}
+		cPath = path.Join(usr.HomeDir, "."+strings.ToLower(appName)+"rc")
+	}
+
+	if err := loadConfig(appName, cPath); err != nil {
+		return err
+	}
+	if err := saveConfig(appName, cPath); err != nil {
+		return err
+	}
 	flag.Parse()
+	return nil
 }
 
-func loadConfig(configPath string) {
+func loadConfig(appName, configPath string) error {
 	fin, err := os.Open(configPath)
 	if _, ok := err.(*os.PathError); ok {
-		log.Printf("No config file found. Creating %s ...", configPath)
-		return
+		fmt.Fprintf(os.Stderr, "No config file found for %s. Creating %s ...\n", appName, configPath)
+		return nil
 	} else if err != nil {
-		log.Fatalf("Unable to read config file %v: %v", configPath, err)
+		return fmt.Errorf("Unable to read %s config file %v: %v", appName, configPath, err)
 	}
 	defer fin.Close()
 
@@ -52,17 +68,26 @@ func loadConfig(configPath string) {
 			continue
 		}
 	}
+	return nil
 }
 
-func saveConfig(configPath string) {
+func saveConfig(appName, configPath string) error {
 	fout, err := os.Create(configPath)
 	if err != nil {
-		log.Fatalf("Unable to open file %v for writing: %v", configPath, err)
+		return fmt.Errorf("Unable to open %s config file %v for writing: %v", appName, configPath, err)
 	}
 	defer fout.Close()
 
 	writer := bufio.NewWriter(fout)
 	defer writer.Flush()
+
+	// header
+	fmt.Fprintf(writer, "# %s configuration\n# \n", appName)
+	fmt.Fprintln(writer, "# This config has https://github.com/schachmat/ingo syntax.")
+	fmt.Fprintln(writer, "# Empty lines or lines starting with # will be ignored.")
+	fmt.Fprintln(writer, "# All other lines must look like `KEY=VALUE` (without the quotes).")
+	fmt.Fprintf(writer, "# The VALUE must not be enclosed in quotes!\n\n")
+
 	flag.VisitAll(func(f *flag.Flag) {
 		_, usage := flag.UnquoteUsage(f)
 		fmt.Fprintln(writer, "#", strings.Replace(usage, "\n    \t", "\n# ", -1))
@@ -72,7 +97,7 @@ func saveConfig(configPath string) {
 	// if we have obsolete keys left from the old config, preserve them in an
 	// additional section at the end of the file
 	if len(obsoleteKeys) == 0 {
-		return
+		return nil
 	}
 	fmt.Fprintln(os.Stderr, "!!!!!!!!!!")
 	fmt.Fprintln(os.Stderr, "! WARNING: The application was probably updated,")
@@ -83,4 +108,5 @@ func saveConfig(configPath string) {
 	for key, val := range obsoleteKeys {
 		fmt.Fprintf(writer, "%v=%v\n", key, val)
 	}
+	return nil
 }
